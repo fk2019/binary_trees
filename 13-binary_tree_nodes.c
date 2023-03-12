719f8341bd57891a9605cdbd56b66f8e5596c308
fk2019 <fkmuiruri8@gmail.com>
2023-03-12 11:56:16 -0700

    Add nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - measures size of a binary tree with at
+ * least one node
+ * @tree: pointer to root node of tree to measure the size
+ *
+ * Return: size otherwise 0 if tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes;
+
+	nodes = 0;
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	nodes += (tree->left && tree->right) ? 1 : 0;
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+	return (nodes);
+}
