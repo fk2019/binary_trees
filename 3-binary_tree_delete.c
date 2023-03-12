2dda86e3267af59b1f298563b1a13f814c04ad4a
fk2019 <fkmuiruri8@gmail.com>
2023-03-12 06:58:43 -0700

    Add tree delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to root node of tree
+ *
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		free(tree->left);
+		free(tree->right);
+	}
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree->right)
+		binary_tree_delete(tree->right);
+	free(tree);
+}
