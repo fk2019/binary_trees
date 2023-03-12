4c03bb476bb0124c5fe8ffc02552a0c2b762e549
fk2019 <fkmuiruri8@gmail.com>
2023-03-12 07:48:32 -0700

    Edit func

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_left, height_right;
 
-	height_l = tree->l ? 1 + binary_tree_height(tree->l) : 0;
-	height_r = tree->r ? 1 + binary_tree_height(tree->r) : 0;
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (height_l > height_r ? height_l : height_r);
 }
