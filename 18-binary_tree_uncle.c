571f388ca83094b83a89608b56870e12213695eb
fk2019 <fkmuiruri8@gmail.com>
2023-03-14 11:23:12 -0700

    Add uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - find uncle of a node
+ * @node: pointer to node to find uncle
+ *
+ * Return: pointer to uncle node, NULL if node/parent is NULL
+ * or node has no uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grandpa;
+
+	if (node == NULL)
+		return (NULL);
+	grandpa = node->parent->parent;
+	if (grandpa == NULL || grandpa->left == NULL || grandpa->right == NULL)
+		return (NULL);
+	return (node->parent == grandpa->left ? grandpa->right : grandpa->left);
+}
