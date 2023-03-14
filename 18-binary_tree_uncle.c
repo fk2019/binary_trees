f5aa16ab400ab883f4295852da1cab5ae7588140
fk2019 <fkmuiruri8@gmail.com>
2023-03-14 11:25:11 -0700

    Edit for null

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *grandpa;
 
-	if (node == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	grandpa = node->parent->parent;
 	if (grandpa == NULL || grandpa->left == NULL || grandpa->right == NULL)
